26d7521471f39a2e0209ea0d25b5d774c7ebc013
Folagbade Adeusi <zothuras@gmail.com>
2021-02-14 22:25:32 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,6 +2,6 @@
 first_number=2
 second_number=1
 sum=first_number + second_number 
-difference=first_number + second_number 
-product=first_number + second_number 
-qoutient=2 /0
+difference=first_number - second_number 
+product=first_number * second_number 
+qoutient=first_number / second_number 
