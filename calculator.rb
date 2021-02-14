ee2aeb9968496ef9f412ec1aebabf0050fbee789
Folagbade Adeusi <zothuras@gmail.com>
2021-02-14 22:26:15 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
-first_number=2
-second_number=1
+first_number=4
+second_number=2
 sum=first_number + second_number 
 difference=first_number - second_number 
 product=first_number * second_number 
-qoutient=first_number / second_number 
+qoutient= first_number / second_number 
